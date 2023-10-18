7fadf7cb1007dd98d50102412a0cac8eec14134a
Rasel Sharif <raselsharif9@gmail.com>
2023-10-18 15:20:33 +0600

    added get single item from brands

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-const { log } = require('console');
+const cors = require('cors');
 const express = require('express');
 const app = express();
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
+// middlewares
+app.use(cors())
+app.use(express.json())
+
 const user = process.env.DB_USER;
 const pass = process.env.DB_PASS;
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${user}:${pass}@cluster0.iuscecj.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -25,20 +29,39 @@ async function run() {
     await client.connect();
 const database = client.db("electronicsDB");
 const brandCollections = database.collection("brands");
+const carCollections = database.collection("cars");
 
-app.get("/brands", async(req,res)=>{
-    const cursor = brandCollections.find();
-    const result = await cursor.toArray()
-    res.send(result)
+// get all brands form DB
+app.get('/brands',async(req,res)=>{
+  const cursor = brandCollections.find()
+  const result =await cursor.toArray()
+  res.send(result)
+})
+// get single brand
+app.get('/brands/:name',async (req, res)=>{
+  const brandName = req.params.name;
+  const filter = {_id: new ObjectId(brandName)}
+  const result = await brandCollections.findOne(filter)
+  res.send(result)
 })
 // insert brands name
     app.post("/brands",async (req,res)=>{
         const brands = req.body;
-        console.log(brands);
-        // const result = await brandCollections.insertOne(brands)
-        // res.send(result)
+        // console.log(brands);
+        const result = await brandCollections.insertOne(brands)
+        res.send(result)
 
     })
+
+    // ======= CAR CRUD  =======
+    // insert CAR to DB
+    app.post("/cars", async(req,res)=>{
+      const cars = req.body;
+      const result = await carCollections.insertOne(cars)
+      res.send(result)
+      
+    }) 
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
